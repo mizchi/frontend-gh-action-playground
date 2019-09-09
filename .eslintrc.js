862833babd0323853e8f08e89c989597cd330feb
mizchi <miz404@gmail.com>
2019-09-09 20:19:58 +0900

    Implement lint and typecheck

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    "eslint:recommended", // お好きなESLint設定をここに
+    "plugin:prettier/recommended"
+  ],
+  env: {
+    browser: true,
+    node: true
+  },
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module"
+  },
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error"
+  }
+};
