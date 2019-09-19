96b94f1af4b1052a15ab846ee863e870e3c96dc3
mizchi <miz404@gmail.com>
2019-09-20 05:34:29 +0900

    Run e2e test on windows/ie and mac/safari

--- e2e/_helpers.ts
+++ e2e/_helpers.ts
@@ -1,6 +1,7 @@
 import { Server } from "http";
 import path from "path";
 import express from "express";
+import webdriver from "selenium-webdriver";
 
 export async function startServer(port: number = 0): Promise<Server> {
   const app = express();
@@ -8,4 +9,52 @@ export async function startServer(port: number = 0): Promise<Server> {
   let server: Server = null as any;
   await new Promise<Server>(r => (server = app.listen(port, r)));
   return server as Server;
+}
+
+// puppeteer like wrapper
+export class Page {
+  constructor(private _driver: webdriver.WebDriver) {}
+
+  async ensureNoError() {
+    const logs = await this._driver
+      .manage()
+      .logs()
+      .get("browser");
+    const errorLogs = logs.filter((log: any) => log.level.name_ === "SEVERE");
+    if (errorLogs.length > 0) {
+      throw new Error(JSON.stringify(errorLogs));
+    }
+  }
+
+  async getText(selector: string) {
+    const elem = this._driver.findElement(webdriver.By.css(selector));
+    return await elem.getText();
+  }
+
+  async loadUrl(url: string) {
+    await this._driver.get(url);
+  }
+
+  async waitFor(selector: string, timeout = 150000) {
+    await this._driver.wait(
+      webdriver.until.elementLocated(webdriver.By.css(selector)),
+      timeout
+    );
+  }
+
+  async evaluate(code: string) {
+    return this._driver.executeScript(code);
+  }
+
+  async title() {
+    return this._driver.getTitle();
+  }
+
+  async close() {
+    return this._driver.quit();
+  }
+
+  click(selector: string) {
+    this._driver.findElement(webdriver.By.css(selector)).click();
+  }
 }
---
+++ e2e/runWithSelenium.test.ts
@@ -0,0 +1,79 @@
+/* eslint-disable */
+import webdriver from "selenium-webdriver";
+import assert from "assert";
+import path from "path";
+import { Server } from "http";
+import { AddressInfo } from "net";
+import { startServer, Page } from "./_helpers";
+
+jest.retryTimes(3);
+jest.setTimeout(30 * 1000);
+
+let server: Server;
+let port: number;
+let page: Page;
+
+beforeAll(async () => {
+  server = await startServer(4000);
+  port = (server.address() as AddressInfo).port;
+  let capabilities: webdriver.Capabilities = null as any;
+  switch (process.env.BROWSER || "chrome") {
+    case "ie": {
+      // HACK: include IEDriver path by nuget
+      const driverPath = path.join(
+        __dirname,
+        "../Selenium.WebDriver.IEDriver.3.150.0/driver/"
+      );
+      process.env.PATH = `${process.env.PATH};${driverPath};`;
+      capabilities = webdriver.Capabilities.ie();
+      capabilities.set("ignoreProtectedModeSettings", true);
+      capabilities.set("ignoreZoomSetting", true);
+      break;
+    }
+    case "firefox": {
+      capabilities = webdriver.Capabilities.firefox();
+      break;
+    }
+    case "safari": {
+      capabilities = webdriver.Capabilities.safari();
+      break;
+    }
+    case "chrome": {
+      capabilities = webdriver.Capabilities.chrome();
+      capabilities.set("chromeOptions", {
+        args: [
+          "--headless",
+          "--no-sandbox",
+          "--disable-gpu",
+          "--window-size=1980,1200"
+        ]
+      });
+      break;
+    }
+  }
+  const driver = await new webdriver.Builder()
+    .withCapabilities(capabilities)
+    .build();
+  page = new Page(driver);
+});
+
+afterAll(async () => {
+  page && (await page.close());
+  server.close();
+});
+
+it("Google", async () => {
+  await page.loadUrl(`http://google.com`);
+  assert.equal(await page.title(), "Google");
+});
+
+it("Title is 'test'", async () => {
+  await page.loadUrl(`http://localhost:${port}/`);
+  assert.equal(await page.title(), "test");
+});
+
+it("Root content is 'hello'", async () => {
+  await page.loadUrl(`http://localhost:${port}/`);
+  const value = await page.getText("[data-testid='root']");
+  assert.equal(value, "hello");
+});
